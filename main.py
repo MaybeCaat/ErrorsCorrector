5c4610d46e6ba4000e69f03a1636ac35ae08c31c
MaybeCaat <vladislav.osipov.023@yandex.ru>
2022-11-01 20:31:54 +0300

    refactor code

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from autocorrect import Speller
 spell = Speller('ru')
-print(spell(input()))
+
+
+def check_word(word):
+    import time
+
+    start_time = time.time()
+    print(spell(word))
+    end_time = time.time()
+    print(f'Время выполнения программы: {end_time - start_time} \n')
+
+
+def corrector():
+    while True:
+        input_word = input('Введите слово (чтобы выйти введите "выход"): ')
+        if input_word == 'выход':
+            return
+        check_word(input_word)
+
+
+if __name__ == '__main__':
+    corrector()
