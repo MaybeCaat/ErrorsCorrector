35fa84f90a3c4522273f8e9a399b268c22f393c0
MaybeCaat <iemm@rambler.ru>
2022-11-03 00:03:34 +0300

    add print writing rule

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # On english
 Project "Errors Corrector". Sirius College.
 
-Bot link: https://t.me/ru_corrector_bot
+My bot link: https://t.me/ru_corrector_bot
 
 
 ## Warning
 1) Add config.py with BOT_TOKEN or add token in main.py
-2) Change language in "dictionary" in main.py
+2) Bot use russian language
 
 
 
@@ -14,12 +14,12 @@ Bot link: https://t.me/ru_corrector_bot
 # На русском
 Проект "Проверка орфорграфии". Колледж "Сириус".
 
-Ссылка на бота: https://t.me/ru_corrector_bot
+Ссылка на моего бота: https://t.me/ru_corrector_bot
 
 
 ## Предупреждение
 1) Добавьте токен бота в переменную BOT_TOKEN в config.py или напрямую в main.py
-2) По умолчанию в боте стоит русский язык. Чтобы он работал - переместите 2 файла из папки localize
+2) Бот работает только с русским языком. Но чтобы он работал - переместите 2 файла из папки localize
 в папку с библиотекой. 
     Если Вы не используете venv, папка: C:\...\Python\Python36\site-packages\enchant\data\mingw64\share\enchant\hunspell
     Если Вы используете venv, папка: ...\venv\Lib\site-packages\enchant\data\mingw64\share\enchant\hunspell
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import time
 import logging
 import enchant
+import requests
 from aiogram import Bot, Dispatcher, executor, types
 from config import BOT_TOKEN
+from bs4 import BeautifulSoup
 
+# не забыть перекинуть файлы локализации
 dictionary = enchant.Dict('ru_RU')
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
@@ -11,10 +14,17 @@ dp = Dispatcher(bot)
 
 
 # Исправление слова и засечение времени
-def check_word(word):
+async def check_word(word):
     start_time = time.time()
 
-    result = dictionary.suggest(word)
+    result_word = dictionary.suggest(word)
+    # ЧТОБЫ РАБОТАЛО С РУССКИМИ СЛОВАМИ
+    print(f'https://gramotei.online/how-to-spell?keyword={result_word[0]}')
+    res = requests.get(f'https://gramotei.online/how-to-spell?keyword={result_word[0]}')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    elem = soup.find("div", class_='hidden-xs col-sm-8 border-bottom text-muted search-item')
+    rule = elem.string
+    result = [result_word, rule]
 
     end_time = time.time()
     print(f'Время исправления слова: {end_time - start_time} секунд')
@@ -23,7 +33,7 @@ def check_word(word):
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    await message.answer("Привет!")
+    await message.answer("Привет! Это бот для проверки орфографии. Введите слово и бот выдаст его правильное написание")
 
 
 @dp.message_handler()
@@ -31,8 +41,8 @@ async def get_word(message: types.Message):
     if dictionary.check(message.text):
         await message.reply('Вы правильно написали слово!')
         return
-    result_word = check_word(message.text)
-    await message.reply(f'Правильное написание Вашего слова: {result_word}')
+    result_word = await check_word(message.text)
+    await message.reply(f'Правильное написание Вашего слова: {result_word[0]}. \nПравило написания: {result_word[1]}')
 
 
 if __name__ == '__main__':
