1db1224e3499ede836630a7431f8b826f2811a01
MaybeCaat <iemm@rambler.ru>
2022-11-03 13:21:40 +0300

    add keyboard with corr. words

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import logging
 import enchant
 import requests
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Text
+
 from config import BOT_TOKEN
 from bs4 import BeautifulSoup
 
@@ -14,26 +16,39 @@ dp = Dispatcher(bot)
 
 
 # Исправление слова и засечение времени
-async def check_word(word):
+async def get_keyboard_corrections(word):
     start_time = time.time()
 
     # получение списка с возможными исправлениями
     result_word = dictionary.suggest(word)
-    # получение страницы с нужным правилом по первому слову из resu
+    buttons = [
+        types.InlineKeyboardButton(text=el, callback_data=f"word_{el}") for el in result_word
+    ]
+    # row_width=1 - одна кнопка в строчке
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    keyboard.add(*buttons)
+
+    end_time = time.time()
+    print(f'Время исправления слова: {end_time - start_time} секунд')
+    return keyboard
+
+
+# получение правила на слово
+async def get_rule(word):
+    start_time = time.time()
+
     res = requests.get(
         'https://gramotei.online/how-to-spell',
-        params={'keyword': result_word[0]}
+        params={'keyword': word}
     )
     soup = BeautifulSoup(res.text, 'html.parser')
     elem = soup.find("div", class_='hidden-xs col-sm-8 border-bottom text-muted search-item')
     # получение текста правила из элемента
     rule = elem.text
-    # создаём список-пару из слова и правила к нему
-    result = [result_word, rule]
 
     end_time = time.time()
-    print(f'Время исправления слова: {end_time - start_time} секунд')
-    return result
+    print(f'Время поиска правила: {end_time - start_time} секунд')
+    return rule
 
 
 # стартовое сообщение
@@ -48,8 +63,20 @@ async def get_word(message: types.Message):
     if dictionary.check(message.text):
         await message.reply('Вы правильно написали слово!')
         return
-    result_word = await check_word(message.text)
-    await message.reply(f'Правильное написание Вашего слова: {result_word[0]}. \nПравило написания: {result_word[1]}')
+    keyboard = await get_keyboard_corrections(message.text)
+    await message.reply(f'Вы ввели слово {message.text}. \nВозможные исправления Вашего слова: ', reply_markup=keyboard)
+
+
+# хэндлер на выбор одного из скорректированных слов
+@dp.callback_query_handler(Text(startswith="word_"))
+async def callbacks_corr_word(call: types.CallbackQuery):
+    # извлекаем слово из callback_data (формат: word_слово)
+    word = call.data.split("_")[1]
+    # получаем правило с помощью get_rule()
+    rule = await get_rule(word)
+    await call.message.answer(f'Правильное написание: {word} \nПравило: {rule}')
+    # отчитываемся о получении колбэка
+    await call.answer()
 
 
 if __name__ == '__main__':
