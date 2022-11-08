d8042437f906f47471c427b3b086f8990199a814
MaybeCaat <iemm@rambler.ru>
2022-11-08 12:14:13 +0300

    improving logs and update readme

--- README.md
+++ README.md
@@ -1,25 +1,36 @@
-# On english
-Project "Errors Corrector". Sirius College.
+# Errors Corrector / Проверка орфографии
+Sorry for my English. I don't know him very well and I write through a translator.
 
-My bot link: https://t.me/ru_corrector_bot
+My project at Sirius College in Russian. 
+The project is a telegram bot that corrects the received word, offering possible spelling options.
+After the user selects the desired spelling,
+the bot will write (if it finds) the rule by which the word was written.
+
+Мой проект в Колледже "Сириус" по русскому языку.   
+Проект - телеграм-бот, который исправляет полученное слово, предлагая варианты возможного написания.
+После того, как пользователь выберет нужное написание, 
+бот напишет (если найдёт) правило, по которому было написано слово.
+
+## Bot link / Ссылка на бота
+https://t.me/ru_corrector_bot   
+(it may not work - so I turned it off)  
+(может не работать - значит я его выключил)
 
 
-## Warning
-1) Add config.py with BOT_TOKEN or add token in main.py
-2) Bot use russian language
+## Warning / Предупреждение
+In Russian:
+1) Add the bot token to the BOT_TOKEN variable in config.py or directly to main.py
+2) The bot only works with Russian. But to make it work, move 2 files from the localize folder
+to the library folder:
+- If you don't use venv, folder: C:\...\Python\Python36\site-packages\enchant\data\mingw64\share\enchant\hunspell
+- If you use venv, folder: ...\venv\Lib\site-packages\enchant\data\mingw64\share\enchant\hunspell
 
 
-
-
-# На русском
-Проект "Проверка орфорграфии". Колледж "Сириус".
-
-Ссылка на моего бота: https://t.me/ru_corrector_bot
-
-
-## Предупреждение
+На русском:
 1) Добавьте токен бота в переменную BOT_TOKEN в config.py или напрямую в main.py
 2) Бот работает только с русским языком. Но чтобы он работал - переместите 2 файла из папки localize
-в папку с библиотекой. 
-    Если Вы не используете venv, папка: C:\...\Python\Python36\site-packages\enchant\data\mingw64\share\enchant\hunspell
-    Если Вы используете venv, папка: ...\venv\Lib\site-packages\enchant\data\mingw64\share\enchant\hunspell
+в папку с библиотекой:
+- Если Вы не используете venv, папка: C:\...\Python\Python36\site-packages\enchant\data\mingw64\share\enchant\hunspell
+- Если Вы используете venv, папка: ...\venv\Lib\site-packages\enchant\data\mingw64\share\enchant\hunspell
+
+## Another language / Другой язык
--- main.py
+++ main.py
@@ -9,12 +9,17 @@ from aiogram.dispatcher.filters import Text
 from config import BOT_TOKEN
 from bs4 import BeautifulSoup
 
-# Если слово не найдено - попробовать лематизировать в нач. форму и проверить на правило
+# Если слово не найдено - попробовать лематизировать в нач. форму и проверить на правило (проверить, будет ли находить)
 # Пофиксить некоторые элементы в списке изменённых слов
 # (когда в элементе 2 непонятных слова через пробел + непонятные слова)
 # Возможно запилить поддержку предложений (и словосочетаний с значением, например пре-, при-)
 # Проверить новую генерацию списков (если что убрать)
 # СДЕЛАТЬ КРАСИВОЕ ОФОРМЛЕНИЕ
+# возможно разделить на файлы (тогда и логи разделить)
+# проверка, когда ввели чушь (например, только точку)
+# очень много не находит правил (шикарно, не повезло)
+# СДЕЛАТЬ ЛОГИ В КОНСОЛЬ (включение бота)
+# ДОПИСАТЬ README.md
 
 
 # не забыть перекинуть файлы локализации
@@ -23,7 +28,7 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 # настройка логирования
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 handler = logging.FileHandler('logs.log', 'w', 'utf-8')
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -43,17 +48,18 @@ async def get_word(message: types.Message):
     # проверка, что слово изначально правильно написано
     if dictionary.check(message.text):
         await message.reply('Вы правильно написали слово!')
+        logger.info('--------------------------------------------------------')
         logger.info(f'Слово: {message.text}')
         logger.info('Написано правильно')
-        logger.info('--------------------------------------------------------')
         return
     # проверка на то, что вариантов не найдёт
     if not dictionary.suggest(message.text):
         await message.reply('К сожалению, исправления слова не найдены!')
-        logger.info(f'Слово: {message.text}')
-        logger.info('Исправления не найдены')
-        logger.info('--------------------------------------------------------')
+        logger.warning('--------------------------------------------------------')
+        logger.warning(f'Слово: {message.text}')
+        logger.warning('Исправления не найдены')
         return
+    logger.info('--------------------------------------------------------')
     logger.info(f'Слово: {message.text}')
     # получение клавиатуры (отправляем слово в нижнем регистре)
     keyboard = await get_keyboard_corrections(message.text.lower())
@@ -90,7 +96,6 @@ async def get_keyboard_corrections(word):
     logger.info(f'Старый список: {dictionary.suggest(word)}')
     logger.info(f'Новый список: {corr_words}')
     logger.info(f'Время исправления слова: {end_time - start_time} секунд')
-    logger.info('--------------------------------------------------------')
     return keyboard
 
 
@@ -119,6 +124,9 @@ async def get_rule(word):
     soup = BeautifulSoup(response.text, 'html.parser')
     # проверка, если правило на сайте не найдено
     if soup.blockquote:
+        logger.warning('--------------------------------------------------------')
+        logger.warning(f'Слово: {word}')
+        logger.warning(f'Правило не найдено')
         return rule
     # берём все слова
     word_rows = soup.find_all("div", class_='col-xs-12 col-sm-4 border-bottom search-item')
@@ -128,10 +136,10 @@ async def get_rule(word):
             rule = row.small.text.lower()
 
     end_time = time.time()
+    logger.info('--------------------------------------------------------')
     logger.info(f'Слово: {word}')
     logger.info(f'Правило: {rule}')
     logger.info(f'Время поиска правила: {end_time - start_time} секунд')
-    logger.info('--------------------------------------------------------')
     return rule
 
 
