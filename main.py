e476eedc8ff596c0632bc4a20f3b14ca706223e8
MaybeCaat <iemm@rambler.ru>
2022-11-08 11:03:27 +0300

    fix logs

--- main.py
+++ main.py
@@ -9,21 +9,55 @@ from aiogram.dispatcher.filters import Text
 from config import BOT_TOKEN
 from bs4 import BeautifulSoup
 
-
 # Если слово не найдено - попробовать лематизировать в нач. форму и проверить на правило
-# Проверка если введены лишние символы (по типу / убрать, а _ заменить на любую букву (надо протестить)
 # Пофиксить некоторые элементы в списке изменённых слов
 # (когда в элементе 2 непонятных слова через пробел + непонятные слова)
-# Возможно запилить поддержку предложений
+# Возможно запилить поддержку предложений (и словосочетаний с значением, например пре-, при-)
 # Проверить новую генерацию списков (если что убрать)
-# РЕФАКТОР НАЗВАНИЙ ПЕРЕМЕННЫХ
+# СДЕЛАТЬ КРАСИВОЕ ОФОРМЛЕНИЕ
 
 
 # не забыть перекинуть файлы локализации
 dictionary = enchant.Dict('ru_RU')
-logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+
+# настройка логирования
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler('logs.log', 'w', 'utf-8')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+# стартовое сообщение
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer("Привет! Это бот для проверки орфографии. Введите слово и бот выдаст его правильное написание")
+
+
+# функция для вывода клавиатуры с возможными вариантами
+@dp.message_handler()
+async def get_word(message: types.Message):
+    # проверка, что слово изначально правильно написано
+    if dictionary.check(message.text):
+        await message.reply('Вы правильно написали слово!')
+        logger.info(f'Слово: {message.text}')
+        logger.info('Написано правильно')
+        logger.info('--------------------------------------------------------')
+        return
+    # проверка на то, что вариантов не найдёт
+    if not dictionary.suggest(message.text):
+        await message.reply('К сожалению, исправления слова не найдены!')
+        logger.info(f'Слово: {message.text}')
+        logger.info('Исправления не найдены')
+        logger.info('--------------------------------------------------------')
+        return
+    logger.info(f'Слово: {message.text}')
+    # получение клавиатуры (отправляем слово в нижнем регистре)
+    keyboard = await get_keyboard_corrections(message.text.lower())
+    await message.reply(f'Вы ввели слово {message.text}. \nВозможные исправления Вашего слова: ', reply_markup=keyboard)
 
 
 # исправление слова и возвращение клавиатуры с возможными вариантами
@@ -53,11 +87,23 @@ async def get_keyboard_corrections(word):
     keyboard.add(*buttons)
 
     end_time = time.time()
-    print(f'Старый список: {dictionary.suggest(word)}')
-    print(f'Новый список: {corr_words}')
-    print(f'Время исправления слова: {end_time - start_time} секунд')
-    print('--------------------------------------------------------')
+    logger.info(f'Старый список: {dictionary.suggest(word)}')
+    logger.info(f'Новый список: {corr_words}')
+    logger.info(f'Время исправления слова: {end_time - start_time} секунд')
+    logger.info('--------------------------------------------------------')
     return keyboard
+
+
+# хэндлер на выбор одного из скорректированных слов
+@dp.callback_query_handler(Text(startswith="word_"))
+async def callbacks_corr_word(call: types.CallbackQuery):
+    # извлекаем слово из callback_data (формат: word_слово)
+    word = call.data.split("_")[1]
+    # получаем правило с помощью get_rule()
+    rule = await get_rule(word)
+    await call.message.answer(f'Правильное написание: {word} \nПравило: {rule}')
+    # отчитываемся о получении колбэка
+    await call.answer()
 
 
 # получение правила на слово
@@ -82,52 +128,11 @@ async def get_rule(word):
             rule = row.small.text.lower()
 
     end_time = time.time()
-    print(f'Слово: {word}')
-    print(f'Правило: {rule}')
-    print(f'Время поиска правила: {end_time - start_time} секунд')
-    print('--------------------------------------------------------')
+    logger.info(f'Слово: {word}')
+    logger.info(f'Правило: {rule}')
+    logger.info(f'Время поиска правила: {end_time - start_time} секунд')
+    logger.info('--------------------------------------------------------')
     return rule
-
-
-# стартовое сообщение
-@dp.message_handler(commands=['start'])
-async def cmd_start(message: types.Message):
-    await message.answer("Привет! Это бот для проверки орфографии. Введите слово и бот выдаст его правильное написание")
-
-
-# функция для вывода клавиатуры с возможными вариантами
-@dp.message_handler()
-async def get_word(message: types.Message):
-    # проверка, что слово изначально правильно написано
-    if dictionary.check(message.text):
-        await message.reply('Вы правильно написали слово!')
-        print(f'Слово: {message.text}')
-        print('Написано правильно')
-        print('--------------------------------------------------------')
-        return
-    # проверка на то, что вариантов не найдёт
-    if not dictionary.suggest(message.text):
-        await message.reply('К сожалению, исправления слова не найдены!')
-        print(f'Слово: {message.text}')
-        print('Исправления не найдены')
-        print('--------------------------------------------------------')
-        return
-    print(f'Слово: {message.text}')
-    # получение клавиатуры (отправляем слово в нижнем регистре)
-    keyboard = await get_keyboard_corrections(message.text.lower())
-    await message.reply(f'Вы ввели слово {message.text}. \nВозможные исправления Вашего слова: ', reply_markup=keyboard)
-
-
-# хэндлер на выбор одного из скорректированных слов
-@dp.callback_query_handler(Text(startswith="word_"))
-async def callbacks_corr_word(call: types.CallbackQuery):
-    # извлекаем слово из callback_data (формат: word_слово)
-    word = call.data.split("_")[1]
-    # получаем правило с помощью get_rule()
-    rule = await get_rule(word)
-    await call.message.answer(f'Правильное написание: {word} \nПравило: {rule}')
-    # отчитываемся о получении колбэка
-    await call.answer()
 
 
 if __name__ == '__main__':
